b7b221e75fbf5642d353bb7768aa092cecbe2ac8
Bruno Haible <bruno@clisp.org>
2000-07-04 14:36:01 +0000

    Comment.

--- tests/check-translit.bat
+++ tests/check-translit.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 REM Simple check of transliteration facilities.
-REM Usage: check-translit.bat SRCDIR FILE fromcode tocode
+REM Usage: check-translit.bat SRCDIR FILE FROMCODE TOCODE
 
 .\iconv -f %3 -t %4 < %1\%2.%3 > tmp
 fc %1\%2.%4 tmp
