7fdca33e14276cb495d30fdf77fb2c3fb4df74ac
Bruno Haible <bruno@clisp.org>
2001-05-26 00:32:43 +0000

    Regenerated. TCVN can now store the accents.

--- lib/flags.h
+++ lib/flags.h
@@ -83,7 +83,7 @@
 #define ei_tis620_oflags (0)
 #define ei_cp874_oflags (HAVE_QUOTATION_MARKS)
 #define ei_viscii_oflags (0)
-#define ei_tcvn_oflags (0)
+#define ei_tcvn_oflags (HAVE_ACCENTS)
 #define ei_iso646_jp_oflags (0)
 #define ei_jisx0201_oflags (0)
 #define ei_jisx0208_oflags (HAVE_QUOTATION_MARKS)
