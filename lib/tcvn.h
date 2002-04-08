d553237a081a83832df8ee7c3c7d9877d17507b5
Bruno Haible <bruno@clisp.org>
2002-04-08 12:39:19 +0000

    Small optimization.

--- lib/tcvn.h
+++ lib/tcvn.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1999-2001 Free Software Foundation, Inc.
+ * Copyright (C) 1999-2002 Free Software Foundation, Inc.
  * This file is part of the GNU LIBICONV Library.
  *
  * The GNU LIBICONV Library is free software; you can redistribute it
@@ -29,13 +29,12 @@ static const unsigned char tcvn_comb_table[] = {
   0xb0, 0xb3, 0xb2, 0xb1, 0xb4,
 };
 
-static const unsigned short tcvn_2uni_1[32] = {
+static const unsigned short tcvn_2uni_1[24] = {
   /* 0x00 */
   0x0000, 0x00da, 0x1ee4, 0x0003, 0x1eea, 0x1eec, 0x1eee, 0x0007,
   0x0008, 0x0009, 0x000a, 0x000b, 0x000c, 0x000d, 0x000e, 0x000f,
   /* 0x10 */
   0x0010, 0x1ee8, 0x1ef0, 0x1ef2, 0x1ef6, 0x1ef8, 0x00dd, 0x1ef4,
-  0x0018, 0x0019, 0x001a, 0x001b, 0x001c, 0x001d, 0x001e, 0x001f,
 };
 static const unsigned short tcvn_2uni_2[128] = {
   /* 0x80 */
@@ -73,7 +72,7 @@ tcvn_mbtowc (conv_t conv, ucs4_t *pwc, const unsigned char *s, int n)
   unsigned char c = *s;
   unsigned short wc;
   unsigned short last_wc;
-  if (c < 0x20)
+  if (c < 0x18)
     wc = tcvn_2uni_1[c];
   else if (c < 0x80)
     wc = c;
