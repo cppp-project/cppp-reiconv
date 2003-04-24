411b8e573d2a856162b3838e283b46201382e6df
Bruno Haible <bruno@clisp.org>
2003-04-24 10:49:10 +0000

    Better support for FreeBSD.

--- lib/iconv.c
+++ lib/iconv.c
@@ -544,4 +544,18 @@ void iconvlist (int (*do_one) (unsigned int namescount,
 
 int _libiconv_version = _LIBICONV_VERSION;
 
+#if defined __FreeBSD__ && !defined __gnu_freebsd__
+/* GNU libiconv is the native FreeBSD iconv implementation since 2002.
+   It wants to define the symbols 'iconv_open', 'iconv', 'iconv_close'.  */
+#define strong_alias(name, aliasname) _strong_alias(name, aliasname)
+#define _strong_alias(name, aliasname) \
+  extern __typeof (name) aliasname __attribute__ ((alias (#name)));
+#undef iconv_open
+#undef iconv
+#undef iconv_close
+strong_alias (libiconv_open, iconv_open)
+strong_alias (libiconv, iconv)
+strong_alias (libiconv_close, iconv_close)
+#endif
+
 #endif
