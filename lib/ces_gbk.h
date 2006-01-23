2dd206a689e1f79db5ec3dc1352f6c4f0563c3a9
Bruno Haible <bruno@clisp.org>
2006-01-23 13:49:10 +0000

    Comment change.

--- lib/ces_gbk.h
+++ lib/ces_gbk.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1999-2001 Free Software Foundation, Inc.
+ * Copyright (C) 1999-2001, 2005 Free Software Foundation, Inc.
  * This file is part of the GNU LIBICONV Library.
  *
  * The GNU LIBICONV Library is free software; you can redistribute it
@@ -19,7 +19,7 @@
  */
 
 /*
- * GBK, CP936
+ * GBK
  */
 
 static int
