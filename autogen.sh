3cde16583032717a7122e28064bf89009ebb4231
Bruno Haible <bruno@clisp.org>
2008-04-14 11:08:50 +0000

    Switch to autoconf 2.62.

--- autogen.sh
+++ autogen.sh
@@ -4,13 +4,13 @@
 # also regenerates all aclocal.m4, config.h.in, Makefile.in, configure files
 # with new versions of autoconf or automake.
 #
-# This script requires autoconf-2.61 and automake-1.10 in the PATH.
+# This script requires autoconf-2.62 and automake-1.10 in the PATH.
 # It also requires either
 #   - the GNULIB_TOOL environment variable pointing to the gnulib-tool script
 #     in a gnulib checkout, or
 #   - the cvs program in the PATH and an internet connection.
 
-# Copyright (C) 2003-2007 Free Software Foundation, Inc.
+# Copyright (C) 2003-2008 Free Software Foundation, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
