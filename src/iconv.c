941f63afe17adcb01e150e29ddb50c6fd40f1291
Bruno Haible <bruno@clisp.org>
2017-01-30 23:48:37 +0100

    Prepare for version 1.15.

--- src/iconv.c
+++ src/iconv.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2000-2009, 2011-2012, 2016 Free Software Foundation, Inc.
+/* Copyright (C) 2000-2009, 2011-2012, 2016-2017 Free Software Foundation, Inc.
    This file is part of the GNU LIBICONV Library.
 
    This program is free software: you can redistribute it and/or modify
@@ -191,7 +191,7 @@ static void print_version (void)
 {
   printf("iconv (GNU libiconv %d.%d)\n",
          _libiconv_version >> 8, _libiconv_version & 0xff);
-  printf("Copyright (C) %s Free Software Foundation, Inc.\n", "2000-2016");
+  printf("Copyright (C) %s Free Software Foundation, Inc.\n", "2000-2017");
   /* xgettext: no-wrap */
   fputs (_("\
 License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
