35064ed9a843eb76ca3d4d946eb5cccc07fcf7d1
Bruno Haible <bruno@clisp.org>
2018-05-04 21:27:39 +0200

    Simplify code. Drop support for Borland C++ on Windows.

--- lib/iconv.c
+++ lib/iconv.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1999-2008, 2011, 2016 Free Software Foundation, Inc.
+ * Copyright (C) 1999-2008, 2011, 2016, 2018 Free Software Foundation, Inc.
  * This file is part of the GNU LIBICONV Library.
  *
  * The GNU LIBICONV Library is free software; you can redistribute it
@@ -559,7 +559,7 @@ const char * iconv_canonicalize (const char * name)
       /* On systems which define __STDC_ISO_10646__, wchar_t is Unicode.
          This is also the case on native Woe32 systems and Cygwin >= 1.7, where
          we know that it is UTF-16.  */
-#if ((defined _WIN32 || defined __WIN32__) && !defined __CYGWIN__) || (defined __CYGWIN__ && CYGWIN_VERSION_DLL_MAJOR >= 1007)
+#if (defined _WIN32 && !defined __CYGWIN__) || (defined __CYGWIN__ && CYGWIN_VERSION_DLL_MAJOR >= 1007)
       if (sizeof(wchar_t) == 4) {
         index = ei_ucs4internal;
         break;
