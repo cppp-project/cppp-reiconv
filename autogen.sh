551543f6c5c98cbf2eaa5aaf21f18adf1687b21a
Bruno Haible <bruno@clisp.org>
2019-07-02 21:23:34 +0200

    Drop unused macro gl_GLIBC21.

--- autogen.sh
+++ autogen.sh
@@ -72,7 +72,7 @@ done
 for file in config.guess config.libpath config.sub install-sh libtool-reloc mkinstalldirs; do
   cp -p build-aux/$file libcharset/build-aux/$file || exit $?
 done
-for file in codeset.m4 fcntl-o.m4 glibc21.m4 lib-ld.m4 relocatable.m4 relocatable-lib.m4 visibility.m4; do
+for file in codeset.m4 fcntl-o.m4 lib-ld.m4 relocatable.m4 relocatable-lib.m4 visibility.m4; do
   cp -p srcm4/$file libcharset/m4/$file || exit $?
 done
 
