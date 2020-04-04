3acb1179dd057fa37b733eb2827822f95d1e31c3
Bruno Haible <bruno@clisp.org>
2020-04-04 14:58:34 +0200

    Change the license of the library from LGPL 2.0 to LGPL 2.1.

--- woe32dll/iconv-exports.c
+++ woe32dll/iconv-exports.c
@@ -3,16 +3,16 @@
    Written by Bruno Haible <bruno@clisp.org>, 2006.
 
    This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU Library General Public License as
+   it under the terms of the GNU Lesser General Public License as
    published by the Free Software Foundation; either version 2 of the
    License, or (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Library General Public License for more details.
+   GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Library General Public
+   You should have received a copy of the GNU Lesser General Public
    License along with this program; see the file COPYING.LIB.  If not,
    see <https://www.gnu.org/licenses/>.  */
 
