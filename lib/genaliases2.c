a0357c3abc697a63a891b51c7dfe6540a62db780
ChenPi11 <wushengwuxi-msctinoulk@outlook.com>
2023-07-12 20:53:10 +0800

    Change `int` to 'unsigned short' in canonical headers. 	修改：     .github/workflows/test.yml 	修改：     lib/genaliases.c 	修改：     lib/genaliases2.c 	修改：     lib/generated/canonical.h 	修改：     lib/generated/canonical_aix.h 	修改：     lib/generated/canonical_aix_sysaix.h 	修改：     lib/generated/canonical_dos.h 	修改：     lib/generated/canonical_extra.h 	修改：     lib/generated/canonical_osf1.h 	修改：     lib/generated/canonical_osf1_sysosf1.h 	修改：     lib/generated/canonical_sysaix.h 	修改：     lib/generated/canonical_syshpux.h 	修改：     lib/generated/canonical_sysosf1.h 	修改：     lib/generated/canonical_syssolaris.h 	修改：     lib/generated/canonical_zos.h

--- lib/genaliases2.c
+++ lib/genaliases2.c
@@ -43,7 +43,7 @@ static void emit_alias (FILE* out1, const char* tag, const char* alias, const ch
 
 static void emit_encoding (FILE* out1, FILE* out2, const char* tag, const char* const* names, size_t n, const char* c_name)
 {
-  fprintf(out2,"  (int)(long)&((struct stringpool2_t *)0)->stringpool_%s_%u,\n",tag,counter);
+  fprintf(out2,"  (unsigned short)(long)&((struct stringpool2_t *)0)->stringpool_%s_%u,\n",tag,counter);
   for (; n > 0; names++, n--)
     emit_alias(out1, tag, *names, c_name);
 }
